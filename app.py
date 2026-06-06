53cb17d13438fe50b4c9d0064fc3d97948247407
lowften <135197187+fdlyirhm@users.noreply.github.com>
2026-06-06 13:48:40 +0700

    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -276,5 +276,6 @@ def index():
 
 
 if __name__ == "__main__":
-    print("Buka browser: http://127.0.0.1:5000")
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
